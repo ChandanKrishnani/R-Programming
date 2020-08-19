9744f439025182f5b1604ee4e7c08ee4e1941915
Chandan krishnani <52024480+ChandanKrishnani@users.noreply.github.com>
2020-08-19 15:26:13 +0530

    Create InputFromUser.r
    Basic input from the user to enter the value from user and print them

---
+++ InputFromUser.r
@@ -0,0 +1,5 @@
+my.name<-readline(propmpt="Enter name :")
+my.age<-readline(prompt="Enter age :")
+
+my.age<-as.integer(my.age)
+print(paste("Hi,"my.name,"next year you will be",my.age+1,"year old.")).r
